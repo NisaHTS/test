5282b52ad434bec026f6c8836cd1e35e46746086
NisaHTS <92494516+NisaHTS@users.noreply.github.com>
2022-01-21 23:08:13 +0700

    Update and rename README.md to inputdata_siswa.php

---
+++ inputdata_siswa.php
@@ -0,0 +1,23 @@
+<a href="index.php">Home</a><br>
+<form action="save.php" method="POST">
+  <table width="25%" border="0">
+  <tr>
+      <td>ID Siswa</td><td><input type="text" name="id_siswa"></td>
+    </tr>
+    <tr>
+      <td>Nama Siswa</td><td><input type="text" name="nama_siswa"></td>
+    </tr>
+    <tr>
+      <td>Jenis Kelamin</td><td><input type="text" name="jenis_kelamin"></td>
+    </tr>
+    <tr>
+      <td>Alamat</td><td><textarea type="text" name="alamat"></textarea></td>
+    </tr>
+    <tr>
+      <td>Hoby</td><td><input type="text" name="hoby"></td>
+    </tr>
+    <tr>
+    <td><input type="submit" name="save" value="save"></td>
+</tr>
+  </table>
+</form>
